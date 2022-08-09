07f877fb8c6d609ca518fc9c2a75e161da3053f2
Lane Miles <lane.m.miles@gmail.com>
2022-08-08 23:40:08 -0400

    fix effects

--- src/RotatingText.tsx
+++ src/RotatingText.tsx
@@ -2,23 +2,24 @@ import './App.css';
 import { useState, useEffect } from 'react';
 
 const OPTIONS = ["talk politics", "play soccer", "eat ice cream", "write code"]
+const START_IDX = Math.round(Math.random() * (OPTIONS.length - 1))
 
 function RotatingText() {
-  const randIdx = Math.round(Math.random() * (OPTIONS.length - 1))
-  const [currentIdx, setCurrentIdx] = useState(randIdx);
+  
+  const [currentIdx, setCurrentIdx] = useState(START_IDX);
   const [isVisible, setIsVisible] = useState(true);
 
   useEffect(() => {
     setTimeout(() => {
+      setIsVisible(false);
+    }, 2000);
+    
+    setTimeout(() => {
       setCurrentIdx((currentIdx + 1) % OPTIONS.length);
-    }, 4000);
+      setIsVisible(true);
+    }, 4000);    
   }, [currentIdx]);
 
-  useEffect(() => {
-    setTimeout(() => {
-      setIsVisible(!isVisible);
-    }, 2000);
-  }, [isVisible]);
 
   const className = "rotating-text" + (isVisible ? "" : " invisible")
 
